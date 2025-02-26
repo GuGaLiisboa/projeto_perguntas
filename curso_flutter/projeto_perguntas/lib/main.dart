a93f131b68cb19670b68a18911aa78f4d5008472
Gustavo <gugaliisboa2@gmail.com>
2025-02-26 20:59:35 -0300

    Commit 2.2 - Commit da aula do dia 26.02

--- curso_flutter/projeto_perguntas/lib/main.dart
+++ curso_flutter/projeto_perguntas/lib/main.dart
@@ -1,38 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:projeto_perguntas/questao.dart';
 import 'package:projeto_perguntas/resposta.dart';
-
+ 
 void main() => runApp(const PerguntaApp());
-
+ 
 class PerguntaAppState extends State<PerguntaApp> {
   var perguntaSelecionada = 0;
-
+  final List<Map<String, Object>> _perguntas = const[
+      {
+        'texto': 'Qual é a sua cor favorita?',
+        'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+      },
+      {
+        'texto': 'Qual é o seu animal favorito?',
+        'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+      },
+      {
+        'texto': 'Qual é o seu instrutor favorito?',
+        'respostas': ['Maria', 'João', 'Leo', 'Beto'],
+      },
+    ];
+ 
+ 
   void responder() {
     setState(() {
       perguntaSelecionada++;
     });
+    print(perguntaSelecionada);    
   }
-
+ 
+  bool get temPerguntaSelecionada {
+    return perguntaSelecionada < _perguntas.length;
+  }
+ 
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      'Qual a sua cor favorita?',
-      'Qual o seu animal favorito?',
-      'Qual time de futebol você torce?',
-      'Qual sua linguagem de programação favorita?',
-    ];
-
+    
+    List<String> respostas = temPerguntaSelecionada
+        ? _perguntas[perguntaSelecionada]['respostas'] as List<String>
+        : [];
+ 
+    //for (String textoResp in perguntas[perguntaSelecionada]['respostas'] as List<String>) {
+    //respostas.add(Resposta(textoResp, responder));
+    //}
+ 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('Perguntas')),
-        body: Column(
-          children: [
-            Questao(perguntas[perguntaSelecionada]),
-            Resposta("Resposta1", responder),
-            Resposta("Resposta2", responder),
-            Resposta("Resposta3", responder),
-          ],
+        appBar: AppBar(
+          title: const Text('Perguntas'),
         ),
+        body: 
+        temPerguntaSelecionada ? Column(
+          children: [
+            Questao(_perguntas[perguntaSelecionada]['texto'] as String),
+            ...respostas.map((t) => Resposta(t, responder)).toList(),
+          ],
+        ) : const Text("Parabéns! Você chegou ao fim do jogo."),
       ),
     );
   }
@@ -40,7 +63,7 @@ class PerguntaAppState extends State<PerguntaApp> {
 
 class PerguntaApp extends StatefulWidget {
   const PerguntaApp({super.key});
-
+ 
   @override
   PerguntaAppState createState() {
     return PerguntaAppState();
